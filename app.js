269134b8332191f16e675f4d0235066e28bde641
BiswasAri10 <bis.ips21@gmail.com>
2023-05-03 13:48:49 +0530

    DOM Manipulation

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('I love Git');
+console.dir(document.domain);
+console.log(document.URL);
+var HeaderTitle = document.getElementById('header-title');
+/*HeaderTitle.textContent = 'Hello';
+HeaderTitle.innerText = 'GoodBye';*/
+HeaderTitle.style.borderBottom = 'solid 3px black';
+let title = document.getElementById("header-title");
+title.style.border = "1px solid black";
+
+let addItemButton = document.querySelector("#main form .btn");
+addItemButton.style.color = "green";
+addItemButton.style.fontWeight = "bold";
